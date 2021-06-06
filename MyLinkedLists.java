22d5549b26ac627e09a5c3956612673165e70f68
patelashok <ashok.patel2917@gmail.com>
2021-06-06 20:09:07 +0530

    delete element then seq 56>30>40>70--AFTER--56>30>70

---
+++ MyLinkedLists.java
@@ -0,0 +1,123 @@
+package com.linklist;
+
+public class MyLinkedLists {
+
+    public INode tail;
+    public INode head;
+
+    //created constructor
+    public MyLinkedLists() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void add(INode newNode) {
+        if(this.head == null && this.tail == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            INode tempNode = this.head;
+            this.head = newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+
+
+    //Append Method
+    public void append(INode newNode) {
+        if(this.head == null && this.tail == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail.setNext(newNode);
+            this.tail = newNode;
+        }
+    }
+    public void insert(INode myNode , INode newNode) {
+        /* store the node in tempNode
+         * Initialize previous node to point new node
+         * make new node point to tempNode
+         */
+        INode tempNode = myNode.getNext();
+        myNode.setNext(newNode);
+        newNode.setNext(tempNode);
+
+    }
+
+    //Deleting first element
+    public INode deleteElement() {
+        INode tempNode = this.head;
+        this.head = head.getNext();
+        return tempNode;
+    }
+    //Deleting last element
+    public INode deleteLastElement() {
+        INode tempNode = this.head;
+        while (!tempNode.getNext().equals(tail)) {
+            tempNode = tempNode.getNext();
+        }
+        this.tail = tempNode;
+        tempNode = tempNode.getNext();
+        return tempNode;
+    }
+    //Search for a particular node
+    public  INode searchNode(INode keyNode) {
+        INode tempNode = this.head;
+        while (!tempNode.getNext().equals(keyNode)) {
+            tempNode = tempNode.getNext();
+        }
+        return keyNode;
+    }
+
+    //Search for a Particular Node and Insert new Element after that
+    public void searchNodeAndInsert(INode keyNode, INode newNode)
+    {
+        INode tempNode = this.head;
+        while(!tempNode.getNext().equals(keyNode)) {
+            tempNode = tempNode.getNext();
+        }
+        INode newtempNode = keyNode.getNext();
+        keyNode.setNext(newNode);
+        newNode.setNext(newtempNode);
+    }
+
+    //Delete a specific Node and show size of the list
+    public INode deleteNodeInBetween(INode keyNode) {
+        INode node1 = this.head;
+        INode tempNode = this.head;
+        while(tempNode != keyNode) {
+            tempNode = tempNode.getNext();
+        }
+        this.head = tempNode;
+        deleteElement();
+        INode node2 = this.head;
+        this.head = node1;
+        INode newtempNode = this.head;
+        while(newtempNode.getNext() != keyNode) {
+            newtempNode = newtempNode.getNext();
+        }
+        newtempNode.setNext(node2);
+        return this.head;
+    }
+
+    public int myListSize() {
+        int size = 0;
+        if(this.head == null)
+            return size;
+        else
+            size = 1;
+        INode tempNode = this.head;
+        while(tempNode.getNext() != null) {
+            tempNode = tempNode.getNext();
+            size++;
+        }
+        return size;
+    }
+
+    public void printMyNodes() {
+        System.out.println("My Nodes: " + head);
+    }
+
+}
---
+++ MyNode.java
@@ -0,0 +1,45 @@
+package com.linklist;
+
+public class MyNode<K> implements INode<K> {
+
+    //Declaring key and next
+    private K key;
+    private INode<K> next;
+
+
+    public MyNode(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    @Override
+    public K getKey() {
+        return null;
+    }
+
+    @Override
+    public void setKey(K key) {
+        this.key = key;
+
+    }
+
+    //getting the get and set functions
+    public INode<K> getNext() {
+        return next;
+    }
+
+    public void setNext(INode<K> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder myNodeString = new StringBuilder();
+        myNodeString.append("My Nodes{" + "keys=")
+                .append(key).append('}');
+        if (next != null)
+            myNodeString.append("->").append(next);
+        return myNodeString.toString();
+    }
+
+}
